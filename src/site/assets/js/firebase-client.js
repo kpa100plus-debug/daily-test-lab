709425d5593a4aa56cda5de74e0f783fcf99a969
kpa100plus-debug <kpa100plus@gmail.com>
2026-08-23 16:34:54 +0900

    feat: add src/site/assets/js/firebase-client.js

---
+++ src/site/assets/js/firebase-client.js
@@ -0,0 +1,28 @@
+import { firebaseConfig, isFirebaseConfigured } from './firebase-config.js';
+
+const sdkVersion = '12.18.0';
+let servicesPromise;
+
+export function getFirebaseServices() {
+  if (!isFirebaseConfigured()) {
+    throw new Error('Firebase web configuration is not connected yet.');
+  }
+
+  servicesPromise ??= Promise.all([
+    import(`https://www.gstatic.com/firebasejs/${sdkVersion}/firebase-app.js`),
+    import(`https://www.gstatic.com/firebasejs/${sdkVersion}/firebase-auth.js`),
+    import(`https://www.gstatic.com/firebasejs/${sdkVersion}/firebase-firestore.js`)
+  ]).then(([appSdk, authSdk, firestoreSdk]) => {
+    const app = appSdk.initializeApp(firebaseConfig);
+    return {
+      app,
+      auth: authSdk.getAuth(app),
+      db: firestoreSdk.getFirestore(app),
+      authSdk,
+      firestoreSdk
+    };
+  });
+
+  return servicesPromise;
+}
+
